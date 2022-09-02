6313e9b6e6d6d951e4735ab08b4e93a651237571
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-02 22:52:31 +0600

    show all category

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const loadAllCategory = async () => {
+  try {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.data.news_category;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const displayAllCategories = async () => {
+  const allCategories = await loadAllCategory();
+
+  const categoryContainer = document.getElementById('category-container');
+
+  allCategories.forEach((category) => {
+    console.log(category);
+
+    const categoryItem = document.createElement('li');
+    categoryItem.innerHTML = category.category_name;
+
+    categoryContainer.appendChild(categoryItem);
+  });
+};
+
+displayAllCategories();
