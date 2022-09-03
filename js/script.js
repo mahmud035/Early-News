7673c1a98d9b6f0c620c8e4a523c2c16c96fe926
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-03 12:49:47 +0600

    get data when one category item clicked and then sorted it based on total_views

--- js/script.js
+++ js/script.js
@@ -11,19 +11,43 @@ const loadAllCategory = async () => {
   }
 };
 
-const displayAllCategories = async () => {
+const displayAllCategories = async (e) => {
   const allCategories = await loadAllCategory();
-
-  const categoryContainer = document.getElementById('category-container');
+  const categorySection = document.getElementById('category-section');
 
   allCategories.forEach((category) => {
-    console.log(category);
+    // console.log(category);
+    const categoryContainer = document.createElement('div');
 
-    const categoryItem = document.createElement('li');
-    categoryItem.innerHTML = category.category_name;
+    categoryContainer.innerHTML = `
+    <li class="item" onclick="loadEachCategory('${category.category_id}')"> ${category.category_name} </li>`;
 
-    categoryContainer.appendChild(categoryItem);
+    categorySection.appendChild(categoryContainer);
   });
 };
 
 displayAllCategories();
+
+const loadEachCategory = async (id) => {
+  try {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${id}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayEachCategory(data.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const displayEachCategory = async (data) => {
+  //* sort the array of objects by the total_view values
+  const sortedArray = data.sort((a, b) => {
+    return b.total_view - a.total_view;
+  });
+  // console.log(sortedArray);
+
+  sortedArray.forEach((news) => {
+    console.log(news);
+  });
+};
